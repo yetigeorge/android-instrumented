11b3be4db56d6e1baacd3eae32c24627e475d36c
Emilia George (Student) <yetigeorge@yahoo.co.uk>
2024-12-05 13:18:35 -0500

    Create MainActivityInstrumentedTest.kt File and Class

---
+++ android2-project2-instrumentedtesting/app/src/androidTest/java/com/example/android/testing/espresso/BasicSample/MainActivityInstrumentedTest.kt
@@ -0,0 +1,18 @@
+package com.example.android.testing.espresso.BasicSample
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.replaceText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityInstrumentedTest {
+
+    
+}
